725cc9c3b74015f6ff8c6600ceaea5c6aea3552c
mizmorr <safonov.ilya22@gmail.com>
2022-05-19 23:58:59 +0300

    task11(down) - done

--- lab11/lab12.pl
+++ lab11/lab12.pl
@@ -1,3 +1,4 @@
+%task11
 even(X):-X1 is X mod 2, X1==0.
 non(X):-X\=0.
 g_c_d(X,Y,Res):-
@@ -11,3 +12,9 @@ sumUp(X,Sum):-sumUp(X,X,Sum),!.
 sumUp(_,0,0):-!.
 sumUp(X,Del,Sum):-D1 is Del-1, sumUp(X,D1,NewSum),
     (non(D1),even(D1),g_c_d(D1,X,Res),1 is Res, Sum is NewSum+1; Sum is NewSum).
+
+sumdown(X,Res):-sumDown(X,X,Res,0),!.
+sumDown(_,0,Res,Res):-!.
+sumDown(X,D,Sum,CurSum):-D1 is D-1,
+    (non(D1),even(D1),g_c_d(D1,X,G),1 is G, NewSum is CurSum+1; NewSum is CurSum),
+    sumDown(X,D1,Sum,NewSum).
